28dc3cc74d1739780a94145c665b10a82d2f06fe
Simon <lisen827@gmail.com>
2018-09-25 02:22:25 +0800

    Add project files.

---
+++ MainframeProgramAnalysisTool/Controllers/BuildController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace MainframeProgramAnalysisTool.Controllers
+{
+    public class BuildController : Controller
+    {
+        // GET: Build
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public void GetDataByFileName(string filename)
+        {
+            string json = string.Empty;
+            using (FileStream fs = new FileStream(Server.MapPath("~/Generated/" + filename + ".txt"), FileMode.Open, System.IO.FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (StreamReader sr = new StreamReader(fs, Encoding.GetEncoding("gb2312")))
+                {
+                    json = sr.ReadToEnd().ToString();
+                }
+            }
+            
+            //get rid of space, linebreak, etc.
+            string pa = @"[\t\n\r\s]";
+            string json_raw = System.Text.RegularExpressions.Regex.Replace(json, pa, "", System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+           
+            object jsonObj = JsonConvert.SerializeObject(json_raw);
+            Response.Write(jsonObj);
+            Response.End();
+        }
+
+    }
+}
---
+++ MainframeProgramAnalysisTool/Scripts/main.js
@@ -0,0 +1,121 @@
+﻿//clear the canvas
+function clearChart() {
+    $('#chart-container').empty();
+}
+
+//nav bar button listeners
+
+$(document).on('click', '#clearChart', function () {
+    event.stopPropagation();
+    event.preventDefault();   
+    clearChart();
+});
+
+$(document).on('click', '#submit-build-new', function () {
+    event.stopPropagation();
+    event.preventDefault();    
+    $('#myModal').modal('hide')
+    clearChart();  
+
+    var datasource = {
+        
+    };
+
+    $.ajax({
+        url: "/Build/GetDataByFileName?filename=seba000",
+        type: 'GET',
+        dataType: 'json',
+        success: function (result) {
+            var jsonResult = JSON.parse(result);
+            $('#chart-container').orgchart({
+                'data': jsonResult,
+                'nodeContent': 'title',//'title'
+                'direction': 'l2r'
+            });
+        }        
+    });
+
+    //var datasource = {
+    //    'name': 'Lao Lao',
+    //    'title': 'general manager',
+    //    'children': [
+    //        { 'name': 'Bo Miao', 'title': 'department manager' },
+    //        {
+    //            'name': 'Su Miao', 'title': 'department manager',
+    //            'children': [
+    //                { 'name': 'Tie Hua', 'title': 'senior engineer' },
+    //                {
+    //                    'name': 'Hei Hei', 'title': 'senior engineer',
+    //                    'children': [
+    //                        { 'name': 'Dan Dan', 'title': 'engineer' }
+    //                    ]
+    //                },
+    //                {
+    //                    'name': 'Pang Pang', 'title': 'senior engineer',
+    //                    'children': [
+    //                        { 'name': 'Bo Miao', 'title': 'department manager' },
+    //                        {
+    //                            'name': 'Su Miao', 'title': 'department manager',
+    //                            'children': [
+    //                                { 'name': 'Tie Hua', 'title': 'senior engineer' },
+    //                                {
+    //                                    'name': 'Hei Hei', 'title': 'senior engineer',
+    //                                    'children': [
+    //                                        {
+    //                                            'name': 'Dan Dan', 'title': 'engineer',
+    //                                            'children': [
+    //                                                { 'name': 'Bo Miao', 'title': 'department manager' },
+    //                                                {
+    //                                                    'name': 'Su Miao', 'title': 'department manager',
+    //                                                    'children': [
+    //                                                        { 'name': 'Tie Hua', 'title': 'senior engineer' },
+    //                                                        {
+    //                                                            'name': 'Hei Hei', 'title': 'senior engineer',
+    //                                                            'children': [
+    //                                                                { 'name': 'Dan Dan', 'title': 'engineer' }
+    //                                                            ]
+    //                                                        },
+    //                                                        {
+    //                                                            'name': 'Pang Pang', 'title': 'senior engineer',
+    //                                                            'children': [
+    //                                                                { 'name': 'Bo Miao', 'title': 'department manager' },
+    //                                                                {
+    //                                                                    'name': 'Su Miao', 'title': 'department manager',
+    //                                                                    'children': [
+    //                                                                        { 'name': 'Tie Hua', 'title': 'senior engineer' },
+    //                                                                        {
+    //                                                                            'name': 'Hei Hei', 'title': 'senior engineer',
+    //                                                                            'children': [
+    //                                                                                { 'name': 'Dan Dan', 'title': 'engineer' }
+    //                                                                            ]
+    //                                                                        },
+    //                                                                        { 'name': 'Pang Pang', 'title': 'senior engineer' }
+    //                                                                    ]
+    //                                                                },
+    //                                                                { 'name': 'Hong Miao', 'title': 'department manager' }
+    //                                                            ]
+    //                                                        }
+    //                                                    ]
+    //                                                },
+    //                                                { 'name': 'Hong Miao', 'title': 'department manager' }
+    //                                            ]
+    //                                        }
+    //                                    ]
+    //                                },
+    //                                { 'name': 'Pang Pang', 'title': 'senior engineer' }
+    //                            ]
+    //                        },
+    //                        { 'name': 'Hong Miao', 'title': 'department manager' }
+    //                    ]
+    //                }
+    //            ]
+    //        }
+    //    ]
+    //};
+
+
+
+    
+
+});
+
