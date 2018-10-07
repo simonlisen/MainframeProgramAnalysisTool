c0fabaa49de9b1778b4113e1a779cff799277750
Simon <lisen827@gmail.com>
2018-10-07 11:33:54 +0800

    check in

--- MainframeProgramAnalysisTool/Scripts/main.js
+++ MainframeProgramAnalysisTool/Scripts/main.js
@@ -3,6 +3,17 @@ function clearChart() {
     $('#chart-container').empty();
 }
 
+//template of the node
+var nodeTemplate = function (data) {
+   // data.tile.split('-')
+    return `
+        <div class="node-outer" data-toggle="tooltip" data-placement="bottom" data-html="true" title="${data.name} is a good guy./br asdasdasdasdssdfgds<h3>ahahahha</h3>">
+        <div class="title">${data.name}</div>
+        <div class="content">${data.title} </div>
+      `;
+};
+
+
 //nav bar button listeners
 
 $(document).on('click', '#clearChart', function () {
@@ -28,7 +39,11 @@ $(document).on('click', '#submit-build-new', function () {
             $('#chart-container').orgchart({
                 'data': jsonResult,
                 'nodeContent': 'title',//'title'
-                'direction': 'l2r'
+                'direction': 'l2r',
+                'nodeTemplate': nodeTemplate,
+                'initCompleted': function () {
+                    $('[data-toggle="tooltip"]').tooltip();
+                }
             });
         }        
     });
