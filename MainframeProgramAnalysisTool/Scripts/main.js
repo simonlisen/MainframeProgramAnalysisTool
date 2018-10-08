c492a3a5690b0ba9b9fc8f2be608211b27e9da7f
Simon <lisen827@gmail.com>
2018-10-08 14:45:08 +0800

    node detail changes

--- MainframeProgramAnalysisTool/Scripts/main.js
+++ MainframeProgramAnalysisTool/Scripts/main.js
@@ -6,11 +6,19 @@ function clearChart() {
 //template of the node
 var nodeTemplate = function (data) {
    // data.tile.split('-')
-    return `
-        <div class="node-outer" data-toggle="tooltip" data-placement="bottom" data-html="true" title="${data.name} is a good guy./br asdasdasdasdssdfgds<h3>ahahahha</h3>">
-        <div class="title">${data.name}</div>
+    if (data.title.charAt(0) == '1') {
+        return `
+        <div class="node-outer">
+        <div class="title">${data.name}<div class="info-btn btn btn-xs btn-success" data-toggle="modal" data-target="#exampleModal" data-name="${data.name}" data-details="${data.title}">i</div></div>
         <div class="content">${data.title} </div>
       `;
+    } else {
+        return `
+        <div class="node-outer">
+        <div class="title">${data.name}<div class="info-btn btn btn-xs btn-default" data-toggle="modal" data-target="#exampleModal" data-name="${data.name}" data-details="${data.title}">i</div></div>
+        <div class="content">${data.title} </div>
+      `;
+    }   
 };
 
 
@@ -40,14 +48,35 @@ $(document).on('click', '#submit-build-new', function () {
                 'data': jsonResult,
                 'nodeContent': 'title',//'title'
                 'direction': 'l2r',
-                'nodeTemplate': nodeTemplate,
-                'initCompleted': function () {
-                    $('[data-toggle="tooltip"]').tooltip();
-                }
+                'nodeTemplate': nodeTemplate
+               
             });
         }        
     });
     
 
 });
+
+$('#exampleModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var nodeName = button.data('name') // Extract info from data-* attributes
+    var details = button.data('details')
+    var modal = $(this)
+    // Flag $ Rate $ Procs $ Jcls $ Desc
+    var detailFields = details.split('$')
+    if (detailFields.length != 5) {
+        //return error
+    }
+    var flag = detailFields[0];
+    var rate = detailFields[1];
+    var procs = detailFields[2];
+    var jcls = detailFields[3];
+    var desc = detailFields[4];
+
+    modal.find('.modal-title').text('Details of ' + nodeName + 'flag: ' + flag)
+    modal.find('.modal-body #details-rate').val(rate)
+    modal.find('.modal-body #details-procs').val(procs)
+    modal.find('.modal-body #details-jcls').val(jcls)
+    modal.find('.modal-body #details-desc').val(desc)
+})
 
